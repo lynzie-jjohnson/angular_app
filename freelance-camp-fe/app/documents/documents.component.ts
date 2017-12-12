327e3cec4ced892fb5d5b976771295824acb643e
Lynzie Johnson <lynzie.jjohnson@gmail.com>
2017-12-11 21:42:48 -0700

    Implementing api to connect with micro service for documents

--- freelance-camp-fe/app/documents/documents.component.ts
+++ freelance-camp-fe/app/documents/documents.component.ts
@@ -1,36 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Rx';
 import { Document } from './document';
+import { DocumentService } from './document.service';
 
 @Component({
-    moduleId: module.id,
-    selector: 'documents',
-    templateUrl: 'documents.component.html',
-    styleUrls: ['documents.component.css']
+	moduleId: module.id,
+	selector: 'documents',
+	templateUrl: 'documents.component.html',
+	styleUrls: ['documents.component.css'],
+	providers: [ DocumentService ]
 })
+export class DocumentsComponent implements OnInit {
+	pageTitle: string = "Document Dashboard"
+	documents: Document[];
+	errorMessage: string;
+	mode = "Observable";
 
-export class DocumentsComponent {
-    pageTitle: string = "Document Dashboard"
-    documents: Document[] = [
-        {
-            title: "My first doc",
-            decsription: "blah blah blah",
-            file_url: "http://google/com",
-            updated_at: "12/8/17",
-            image_url: "https://d2wucpkmh57zie.cloudfront.net/wp-content/uploads/2015/10/stocksnap.jpg",
-        },
-        {
-            title: "My second doc",
-            decsription: "blah blah blah",
-            file_url: "http://google/com",
-            updated_at: "12/8/17",
-            image_url: "https://cdn.makeawebsitehub.com/wp-content/uploads/2014/12/deathtostockphoto.jpg",
-        },
-        {
-            title: "My third doc",
-            decsription: "blah blah blah",
-            file_url: "http://google/com",
-            updated_at: "12/8/17",
-            image_url: "https://static.pexels.com/photos/7919/pexels-photo.jpg",
-        }
-    ]
+	constructor(
+		private documentService: DocumentService,
+	) {}
+
+	ngOnInit() {
+		let timer = Observable.timer(0, 5000);
+		timer.subscribe(() => this.getDocuments());
+	}
+
+	getDocuments() {
+		this.documentService.getDocuments()
+				.subscribe(
+					documents => this.documents = documents,
+					error => this.errorMessage = <any>error
+				);
+	}
 }
