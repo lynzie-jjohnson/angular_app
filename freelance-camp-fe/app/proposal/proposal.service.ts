6beb058330b979876878bce7228131fb6aae1788
Lynzie Johnson <lynzie.jjohnson@gmail.com>
2017-12-12 10:59:00 -0700

    Implemented show page for proposals

--- freelance-camp-fe/app/proposal/proposal.service.ts
+++ freelance-camp-fe/app/proposal/proposal.service.ts
@@ -6,7 +6,7 @@ import { Proposal } from './proposal';
 
 @Injectable()
 export class ProposalService {
-	private proposalsUrl = 'http://localhost:3002/proposals.json';
+	private proposalsUrl = 'http://localhost:3002/proposals';
 
 	constructor(
 		private http: Http
@@ -16,7 +16,11 @@ export class ProposalService {
 		return this.http.get(this.proposalsUrl)
 										.map((response: Response) => <Proposal[]>response.json())
 										.catch(this.handleError);
-	}
+    }
+    
+    getProposal(id: number) {
+        return this.http.get(this.proposalsUrl + "/" + id + '.json');
+    }
 
 	private handleError (error: Response | any) {
     // In a real world app, we might use a remote logging infrastructure
